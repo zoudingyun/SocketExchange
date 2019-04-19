f91c7d08c41380c3bef52ae52a212a0a4b92ef73
Unknown <zoudingyun@foxmail.com>
2019-04-19 21:07:21 +0800

    ini

---
+++ src/main/java/per/zdy/socketexchange/SocketExchangeApplication.java
@@ -0,0 +1,13 @@
+package per.zdy.socketexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketExchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/per/zdy/socketexchange/SocketExchangeApplicationTests.java
@@ -0,0 +1,16 @@
+package per.zdy.socketexchange;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SocketExchangeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
