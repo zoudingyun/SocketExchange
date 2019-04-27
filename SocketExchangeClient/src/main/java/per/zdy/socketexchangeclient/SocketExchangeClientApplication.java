368e615e88428f23e17d7697e3cf96f6af1253d4
Unknown <zoudingyun@foxmail.com>
2019-04-27 20:41:46 +0800

    图形化

--- SocketExchangeClient/src/main/java/per/zdy/socketexchangeclient/SocketExchangeClientApplication.java
+++ SocketExchangeClient/src/main/java/per/zdy/socketexchangeclient/SocketExchangeClientApplication.java
@@ -10,14 +10,16 @@ import java.awt.*;
 public class SocketExchangeClientApplication {
 
     public static void main(String[] args) {
+        //因为是图形化所以禁用headless模式
+        System.setProperty("java.awt.headless","false");
         SpringApplication.run(SocketExchangeClientApplication.class, args);
 
         JFrame frame = new JFrame();
-        frame.setSize(500 , 1000);
+        frame.setSize(800 , 600);
         //禁用close功能
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        //frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         //不显示标题栏,最大化,最小化,退出按钮
-        frame.setUndecorated(true);
+        //frame.setUndecorated(true);
         //frame.add(view, BorderLayout.CENTER);
         //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setLocationByPlatform(true);
